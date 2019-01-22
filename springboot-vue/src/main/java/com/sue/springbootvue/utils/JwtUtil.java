211d974a89db85c9779dec06fc54c0ad30982778
sue <tanhuiwen0620@gmail.com>
2019-01-22 17:31:36 +0800

    我干了什么

---
+++ springboot-vue/src/main/java/com/sue/springbootvue/utils/JwtUtil.java
@@ -0,0 +1,46 @@
+package com.sue.springbootvue.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtUtil {
+    static final String SECRET = "ThisIsASecret";
+
+    public static String generateToken(String username) {
+        HashMap<String, Object> map = new HashMap<>();
+        //you can put any data in the map
+        map.put("username", username);
+        String jwt = Jwts.builder()
+                .setClaims(map)
+                .setExpiration(new Date(System.currentTimeMillis() + 3600*8))// 8 hour
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
+        return "Bearer "+jwt; //jwt前面一般都会加Bearer
+    }
+
+    public static void validateToken(String token) {
+        try {
+            // parse the token.
+            Map<String, Object> body = Jwts.parser()
+                    .setSigningKey(SECRET)
+                    .parseClaimsJws(token.replace("Bearer ",""))
+                    .getBody();
+        }catch (Exception e){
+            throw new IllegalStateException("Invalid Token. "+e.getMessage());
+        }
+    }
+    public Claims parseToken(String token) throws Exception {
+            Claims claims=Jwts.parser()//得到DefaultJwtParser
+                            .setSigningKey(SECRET)//设置签名的秘钥
+                            .parseClaimsJws(token)
+                            .getBody(); //设置需要解析的jwt
+             return claims;
+            }
+
+}
+
